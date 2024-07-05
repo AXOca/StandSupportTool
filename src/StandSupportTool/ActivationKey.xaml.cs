2e1751712f7d6a15004853cda34f86549369638a
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2024-07-06 01:02:48 +0200

    Improve checks for Activation Key

--- src/StandSupportTool/ActivationKey.xaml.cs
+++ src/StandSupportTool/ActivationKey.xaml.cs
@@ -15,10 +15,20 @@ namespace StandSupportTool
         private void Confirm_Click(object sender, RoutedEventArgs e)
         {
             string activationKey = ActivationKeyText.Text;
+            string validPattern = "Stand-Activate-";
 
-            if (!activationKey.Contains("Stand-Activate"))
+            if (!activationKey.Contains(validPattern))
             {
                 MessageBox.Show("The key you are trying to use doesn't follow the correct pattern, it must contain \"Stand-Activate-\".", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            int startIndex = activationKey.IndexOf(validPattern) + validPattern.Length;
+            string keySuffix = activationKey.Substring(startIndex);
+
+            if (!keySuffix.All(c => char.IsLower(c) || char.IsDigit(c)))
+            {
+                MessageBox.Show("The key suffix must be all lowercase.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
